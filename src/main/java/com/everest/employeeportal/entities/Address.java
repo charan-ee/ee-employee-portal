e04d2fa238b5c24aa0ded74ccfbddaa36eb481c1
Charan <sai.peram@everest.engineering>
2022-02-23 00:20:57 +0530

    added validators to the fields of Employee and Address

--- src/main/java/com/everest/employeeportal/entities/Address.java
+++ src/main/java/com/everest/employeeportal/entities/Address.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -14,10 +15,18 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "Address can't be empty")
     private String addressLine1;
+
     private String addressLine2;
+
+    @NotBlank(message = "Enter you city")
     private String city;
+
     private String state;
-    private Long zipcode;
+
     private String country;
+
+    @NotBlank
+    private Long zipcode;
 }
--- src/main/java/com/everest/employeeportal/entities/Employee.java
+++ src/main/java/com/everest/employeeportal/entities/Employee.java
@@ -3,6 +3,7 @@ package com.everest.employeeportal.entities;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -15,30 +16,42 @@ public class Employee {
     private Long id;
 
     @Column(name = "firstName")
+    @NotBlank(message = "First Name can't be empty")
     private String firstName;
 
     @Column(name = "lastName")
+    @NotBlank(message = "Last Name can't be empty")
     private String lastName;
 
     @Column(name = "password")
+    @NotBlank
+    @Size(min = 5)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])$")
     private String password;
 
+    @Email
     @Column(name = "personalEmail")
+    @NotBlank(message = "Personal Email can't be empty")
     private String personalEmailId;
 
+    @Email
     @Column(name = "everestEmail")
+    @NotBlank(message = "Enter Organisation email")
     private String everestEmailId;
 
     @Column(name = "DOB")
+    @NotBlank
     private LocalDate dob;
 
     @Column(name = "DOJ")
+    @NotBlank
     private LocalDate doj;
 
     @Column(name = "Designation")
     private String designation;
 
     @Column(name = "Experience")
+    @Size(min = 0)
     private Integer experienceInYears;
 
     @Column(name = "Bio")
