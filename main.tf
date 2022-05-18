91a0d5247372a8e2e4ef36089213ff86d4030a54
Charan <sai.peram@everest.engineering>
2022-05-18 16:43:34 +0530

    update terraform.yml

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "employee_app_server" {
--- variables.tf
+++ variables.tf
@@ -12,7 +12,3 @@ variable "ssh_key_public" {
   description = "SSH Public Key to connect EC2"
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYdecKG6lvaRnJ2oSyR+Aj7Z2HwzHeGOESF6dfpd/qkJGvZIFPK2h/1ehx+rUND57uT53h2AqhmSaJ+EzMYdvtxt82tLjIUwTRtUbOqg/lJeEZ8FZStOcPUBMHbd0NIsLMi8zyCguawkl0RkmJ65xulorbxaeY91U0DalxQ29v+4V0P0GTloxGGwK7wVyVK+wRzOYmXXnRPdDK75LpTkpIRtKE5qq4ozzUT8SD3dFcnAmVDte+a35WHRbgAdVHXDvNJuUHhm4m7bdGzgx/i8bKg2AW3WplTSSUMOXlNMSIi+B90go8GchFTwSRnicyS+eTuA9FZwRtrvpMOwOD9m3T/VaxDbzapbNSpJsfEdg0wMzZkK0uW2RwRH/Qlv7S9y8bfctxuishrAZ5GeTzxU6fJdvjsLzUnw5RsfXyLpZX7LYaiTn2R+C6A+e8VpBo2aBynJJrztdbyxiGaThPb3qbHdn5M9chRLb7do0vj5besitrmdoRuVfZauo4hxVyv1M= charan@Charans-MacBook-Pro.local"
 }
-
-variable "aws_access_key_id" {}
-
-variable "aws_secret_key" {}
