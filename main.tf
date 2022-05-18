59b0a45287aa6b45770fa645f6fd566e95d6c16d
Charan <sai.peram@everest.engineering>
2022-05-18 17:16:30 +0530

    updated terraform.yml

--- main.tf
+++ main.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
   required_version = ">= 1.0.4"
 }
 
 provider "aws" {
+  version = "~> 3.0"
   region = var.aws_region
 }
 
